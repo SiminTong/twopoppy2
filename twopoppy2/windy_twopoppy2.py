3662c4e49e3ef2b7c95fdaa1e3183c29ced77202
Simin <astro.stong@gmail.com>
2025-12-29 12:10:55 +0000

    fixed a bug.

--- twopoppy2/windy_twopoppy2.py
+++ twopoppy2/windy_twopoppy2.py
@@ -256,6 +256,8 @@ class Twopoppy_w():
             else:
                 raise NameError(f'The attribute {key} cannot be stored as it does not exist')
             if (key != 'r') & (key != 'ri') & (key !='T_gas'): # Temperature is time-independent.
+                _data = getattr(self, self_key)
+                
                 if self.data[key] is None:
                     self.data[key] = _data
                 else:
